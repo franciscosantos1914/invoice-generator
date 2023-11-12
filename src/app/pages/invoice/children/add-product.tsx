f62f01350a71e9355aecc305a7d401759af610e5
francisco.santos <>
2023-11-12 15:39:37 +0100

    chore: add product page

--- src/app/pages/invoice/children/add-product.tsx
+++ src/app/pages/invoice/children/add-product.tsx
@@ -1,30 +1,20 @@
-"use client"
+"use client";
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useState } from "react";
 
-export default function AddProduct() {
-  let [isOpen, setIsOpen] = useState(true);
+export default function AddProduct(props: {
+  isOpen: boolean;
+  onIsOpenChange: Function;
+}) {
+  let [isOpen, setIsOpen] = useState(props.isOpen);
 
   function closeModal() {
     setIsOpen(false);
-  }
-
-  function openModal() {
-    setIsOpen(true);
+    props.onIsOpenChange(false);
   }
 
   return (
     <>
-      <div className="fixed inset-0 flex items-center justify-center">
-        <button
-          type="button"
-          onClick={openModal}
-          className="rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
-        >
-          Open dialog
-        </button>
-      </div>
-
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={closeModal}>
           <Transition.Child
@@ -53,24 +43,159 @@ export default function AddProduct() {
                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
-                    className="text-lg font-medium leading-6 text-gray-900"
+                    className="flex gap-2 text-lg font-medium leading-6 text-sky-900"
                   >
-                    Payment successful
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z"
+                      />
+                    </svg>
+                    <span className="text-sky-900">Adicionar Produto</span>
                   </Dialog.Title>
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-500">
-                      Your payment has been successfully submitted. We’ve sent
-                      you an email with all of the details of your order.
-                    </p>
+                  <div className="mt-5">
+                    <form>
+                      <div className="space-y-12">
+                        <div className="border-b border-gray-900/10 pb-12">
+                          <div className="columns-2">
+                            <div className="col-span-full">
+                              <label
+                                htmlFor="street-address"
+                                className="block text-sm font-medium leading-6 text-gray-900"
+                              >
+                                Descrição
+                              </label>
+                              <div className="mt-2">
+                                <input
+                                  type="text"
+                                  placeholder="Descrição"
+                                  className="block w-full p-4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                              </div>
+                            </div>
+
+                            <div className="sm:col-span-2">
+                              <label
+                                htmlFor="postal-code"
+                                className="block text-sm font-medium leading-6 text-gray-900"
+                              >
+                                Quantidade
+                              </label>
+                              <div className="mt-2">
+                                <input
+                                  type="text"
+                                  placeholder="Quantidade"
+                                  className="block w-full p-4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                              </div>
+                            </div>
+                          </div>
+
+                          <div className="columns-3 mt-3">
+                            <div className="sm:col-span-2 sm:col-start-1">
+                              <label
+                                htmlFor="city"
+                                className="block text-sm font-medium leading-6 text-gray-900"
+                              >
+                                Preço Unitário
+                              </label>
+                              <div className="mt-2">
+                                <input
+                                  type="number"
+                                  placeholder="Preço Unitário"
+                                  className="block w-full p-4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                              </div>
+                            </div>
+
+                            <div className="sm:col-span-2">
+                              <label
+                                htmlFor="region"
+                                className="block text-sm font-medium leading-6 text-gray-900"
+                              >
+                                Unidade (UN)
+                              </label>
+                              <div className="mt-2">
+                                <input
+                                  type="text"
+                                  placeholder="Unidade (UN)"
+                                  className="block w-full p-4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                              </div>
+                            </div>
+
+                            <div className="sm:col-span-3">
+                              <label
+                                htmlFor="country"
+                                className="block text-sm font-medium leading-6 text-gray-900"
+                              >
+                                Imposto
+                              </label>
+                              <div className="mt-2">
+                                <select
+                                  id="country"
+                                  className="block w-full p-4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                                >
+                                  <option>IVA (14%)</option>
+                                </select>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </form>
                   </div>
 
-                  <div className="mt-4">
+                  <div className="mt-4 flex">
                     <button
                       type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      className="mr-2 flex items-center rounded-md border border-transparent bg-sky-100 px-4 py-2 text-sm font-medium text-sky-900 hover:bg-sky-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-sky-500 focus-visible:ring-offset-2"
                       onClick={closeModal}
                     >
-                      Got it, thanks!
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                      </svg>
+                      Adicionar
+                    </button>
+                    <button
+                      type="button"
+                      className="flex items-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
+                      onClick={closeModal}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                      </svg>
+                      Fechar
                     </button>
                   </div>
                 </Dialog.Panel>
--- src/app/pages/invoice/children/body.tsx
+++ src/app/pages/invoice/children/body.tsx
@@ -1,4 +1,18 @@
+"use client";
+import { useState } from "react";
+import AddProduct from "./add-product";
+
 export default function InvoiceBody() {
+  let [isOpen, setIsOpen] = useState(false);
+
+  function openAddProductModal() {
+    setIsOpen(true);
+  }
+
+  function handleOpenChange(evt: any) {
+    setIsOpen(evt.target.value);
+  }
+
   return (
     <div>
       <div className="pt-10 flex gap-16">
@@ -74,51 +88,12 @@ export default function InvoiceBody() {
                   scope="row"
                   className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                 >
-                  <textarea
-                    cols={20}
-                    rows={3}
-                    className="ring-1 p-3 ring-gray-300 cursor-pointer"
-                    placeholder="Descrição"
-                  ></textarea>
+                  Microsoft Surface Pro
                 </th>
-                <td className="px-6 pb-14">
-                  <input
-                    type="text"
-                    className="h-8 w-24 ring-1 pl-3 ring-gray-300 cursor-pointer"
-                    placeholder="UN"
-                  />
-                </td>
-                <td className="px-6 pb-14">
-                  <input
-                    type="text"
-                    className="h-8 w-32 ring-1 pl-3 ring-gray-300 cursor-pointer"
-                    placeholder="Preço"
-                  />
-                </td>
-                <td className="px-6 pb-14">
-                  <input
-                    type="text"
-                    className="h-8 w-32 ring-1 pl-3 ring-gray-300 cursor-pointer"
-                    placeholder="Montante"
-                  />
-                </td>
+                <td className="px-6 py-4">White</td>
+                <td className="px-6 py-4">Laptop PC</td>
+                <td className="px-6 py-4">No</td>
                 <td className="flex items-center gap-1 px-6 py-4">
-                  <button className="w-7 h-7 flex justify-center items-center rounded-full bg-sky-100 hover:bg-sky-200">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-5 h-5"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                      />
-                    </svg>
-                  </button>
                   <button className="w-7 h-7 flex justify-center items-center rounded-full hover:bg-gray-200">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -126,41 +101,12 @@ export default function InvoiceBody() {
                       viewBox="0 0 24 24"
                       strokeWidth={1.5}
                       stroke="currentColor"
-                      className="w-5 h-5"
+                      className="w-6 h-6"
                     >
                       <path
                         strokeLinecap="round"
                         strokeLinejoin="round"
-                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                      />
-                    </svg>
-                  </button>
-                </td>
-              </tr>
-              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  Microsoft Surface Pro
-                </th>
-                <td className="px-6 py-4">White</td>
-                <td className="px-6 py-4">Laptop PC</td>
-                <td className="px-6 py-4">No</td>
-                <td className="flex items-center gap-1 px-6 py-4">
-                  <button className="w-7 h-7 flex justify-center items-center rounded-full bg-sky-100 hover:bg-sky-200">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-5 h-5"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
                       />
                     </svg>
                   </button>
@@ -186,6 +132,33 @@ export default function InvoiceBody() {
           </table>
         </div>
       </div>
+      <div className="mt-7">
+        <button
+          type="button"
+          className="flex gap-1 bg-sky-100 text-gray-500 rounded-md p-2 font-medium ring-1 ring-sky-100 cursor-pointer hover:bg-sky-200"
+          onClick={openAddProductModal}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z"
+            />
+          </svg>
+          Adicionar Itens na Fatura
+        </button>
+      </div>
+      <AddProduct
+        isOpen={isOpen}
+        onIsOpenChange={handleOpenChange}
+      ></AddProduct>
     </div>
   );
 }
